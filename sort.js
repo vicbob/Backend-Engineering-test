478f7f2048a1ca685160b4c1c301040dba6e6d7c
vicbob <vicbob360@gmail.com>
2020-10-27 20:38:39 +0100

    minor changes to sort

--- sort.js
+++ sort.js
@@ -1,3 +1,28 @@
 const sortByCode = (arr) => {
     arr.sort((a, b) => a.code - b.code)
 }
+
+arr = [
+    {
+        "name":"test 10",
+        "country":"USA",
+        "code":12
+    },
+    {
+        "name":"test 10",
+        "country":"USA",
+        "code":1
+    },
+    {
+        "name":"test 10",
+        "country":"USA",
+        "code":44
+    },
+    {
+        "name":"test 4",
+        "country":"NIGERIA",
+        "code":200
+    },
+]
+sortByCode(arr)
+console.log(arr)
